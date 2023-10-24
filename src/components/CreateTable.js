1016275cbea86930022481fa13582fdc2b89486f
nonnhjleno <taaaisei999@gmail.com>
2023-10-24 14:52:58 +0900

    CreateTableFormのページ遷移を作成。Page 1とPage 2でDataを連動。

--- src/components/CreateTable.js
+++ src/components/CreateTable.js
@@ -1,27 +1,37 @@
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { v4 as uuidv4 } from 'uuid';
 import { currentSelectedDatabaseContext } from '../App.js';
+
+// const CreateTableFirst = () => {
+//   return (
+//     <div>
+//       Page 1
+//     </div>
+//   )
+// }
 
 const CreateTable = () => {
   const currentSelectedDatabase = useContext(currentSelectedDatabaseContext);
   const [page, setPage] = useState(1);
-  const [data, setData] = useState({ name: '', columnNum: 0 });
+  const [data, setData] = useState({ name: '', columnNum: 3 });
 
   const {
     register,
     handleSubmit,
+    // setValue,
     formState: { errors },
-    // getValues,
+    setValue
   } = useForm({
     criteriaMode: 'all',
     defaultValues: {
-      name: '',
-      columnNum: 3,
+      name: data.name,
+      columnNum: data.columnNum,
     },
   });
 
   const handleOnSubmit = formData => {
-    console.log(formData.name, formData.columnNum);
+    // console.log(formData.name, formData.columnNum);
     setData(formData);
     setPage(2);
   };
@@ -29,6 +39,8 @@ const CreateTable = () => {
   const handleOnError = errors => console.log(errors);
 
   if (page === 1) {
+
+    console.log({...register('columnNum')});
     return (
       <form onSubmit={handleSubmit(handleOnSubmit, handleOnError)}>
         <h1>{currentSelectedDatabase}</h1>
@@ -38,6 +50,7 @@ const CreateTable = () => {
             {...register('name', {
               required: 'テーブル前を入力してください',
             })}
+            id='tableName'
             type="text"
             className="border-2 border-black rounded"
           />
@@ -59,14 +72,39 @@ const CreateTable = () => {
       </form>
     );
   } else if (page === 2) {
+    const columnComponents = [];
+
+    for (let i = 0; i < data.columnNum; i++) {
+      columnComponents.push(<Column key={uuidv4()} index={i} />);
+    }
+
     return (
       <div>
         <button onClick={() => setPage(1)}>Page 1に戻る</button>
+        <div id="buttonsArea" className='flex'>
+          <button onClick={() => {
+            const newColumnNum = data.columnNum + 1;
+            setData({ name: data.name, columnNum: newColumnNum });
+            setValue('columnNum',data.columnNum);
+          }}>カラム追加</button>
+          <button onClick={() => {
+            const newColumnNum = data.columnNum > 1 ? data.columnNum - 1 : data.columnNum;
+            setData({ name: data.name, columnNum: newColumnNum });
+            setValue('columnNum',data.columnNum);
+          }}>カラム削除</button>
+        </div>
         <p>テーブル名: {data.name}</p>
         <p>カラムの個数: {data.columnNum}</p>
+        <form className="columns">{columnComponents}</form>
       </div>
     );
   }
 };
+
+const Column = ({ index }) => {
+  return (
+    <p>Column {index + 1}</p>
+  );
+}
 
 export default CreateTable;
