f3817a06b69c9c331a9d07c3f4325aa37afa6ad9
nonnhjleno <taaaisei999@gmail.com>
2023-12-05 11:51:19 +0900

    CreateDatabase関数のhandleSubmit関数のaxiosをfetchに書き換え

--- src/components/CreateDatabase.js
+++ src/components/CreateDatabase.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react'
 
 // TODO: useFormを使って書き直す
@@ -6,16 +5,25 @@ import React from 'react'
 const CreateDatabase = () => {
     const handleSubmit = event => {
 
-        const formData = new FormData(event.target); // フォーム内のデータを取得
-        const name = formData.get("name"); // "name"という名前の入力フィールドの値を取得
-        console.log(name); // 入力フィールドの値をコンソールに出力
-        axios.post(`http://localhost:3000/createDatabase?name=${name}`, {},{headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
+        const formData = new FormData(event.target);
+        const name = formData.get("name");
+        console.log(name);
+
+        fetch('http://localhost:3000/createDatabase', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `name=${name}`
+        })
+            .then(response => response.json())
             .then(result => {
                 console.log(result);
             })
             .catch(error => {
                 console.error("APIでエラーが発生しました", error);
             });
+
     };
 
     return (
