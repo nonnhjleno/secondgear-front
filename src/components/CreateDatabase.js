cb4ec31e08661a2d184f929bc19994c18ba7324d
h3319 <h3319@jmth.local>
2023-12-13 11:32:06 +0900

    CreateDatabaseをuseFormで記述。axiosにasync, awaitを利用した。

--- src/components/CreateDatabase.js
+++ src/components/CreateDatabase.js
@@ -1,35 +1,52 @@
 import axios from 'axios';
 import React from 'react'
-
-// TODO: useFormを使って書き直す
+import { useForm } from 'react-hook-form';
 
 const CreateDatabase = () => {
-    const handleSubmit = event => {
 
-        const formData = new FormData(event.target); // フォーム内のデータを取得
-        const name = formData.get("name"); // "name"という名前の入力フィールドの値を取得
-        console.log(name); // 入力フィールドの値をコンソールに出力
-        axios.post(`http://localhost:3000/createDatabase?name=${name}`, {},{headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
-            .then(result => {
-                console.log(result);
-            })
-            .catch(error => {
-                console.error("APIでエラーが発生しました", error);
-            });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        criteriaMode: 'all',
+    });
+
+    const handleOnError = errors => console.log(errors);
+
+    const handleOnSubmit = async formData => {
+        try {
+            const response = await axios.post(
+                `http://localhost:3000/createDatabase`,
+                formData,
+                {
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                }
+            );
+
+            console.log(response);
+        } catch (error) {
+            console.error("APIでエラーが発生しました", error);
+        }
     };
 
     return (
-        <form onSubmit={event => handleSubmit(event)}>
-            <label htmlFor="name">データベース名:
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className=' border-2'
-                />
-            </label>
-            <input type="submit" defaultValue={"Submit"} />
+        <form onSubmit={handleSubmit(handleOnSubmit, handleOnError)}>
+            <label htmlFor="name">データベース名: </label>
+            <input
+                {...register('name', {
+                    required: 'データベース名を入力してください',
+                })}
+                id='databaseName'
+                type="text"
+                className="border-2 border-black rounded"
+            />
+            {errors.name && <p className="text-red-600">{errors.name.message}</p>}
+            <button type="submit" className="border-2 border-black rounded">
+                決定
+            </button>
         </form>
+
     );
 }
 
