3e8f1f7012a7a0c157f5871a9241622c040a6ff5
nonnhjleno <taaaisei999@gmail.com>
2024-01-04 14:42:50 +0900

    createTableの型の選択肢を別ファイルに記述

--- src/components/CreateTable.js
+++ src/components/CreateTable.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import styleForInput from './styleForInput';
 import axios from 'axios';
 import { currentSelectedDatabaseContext } from '../App';
+import typeChoices from './typeChoices';
 
 const CreateTable = ({ setCreateTableFlag }) => {
 
@@ -100,30 +101,7 @@ const CreateTableField = ({ index, formFields, deleteForm, control }) => {
             className={` ${styleForInput}`}
             // styles={customStyles}
             {...field}
-            options={[
-              { "value": "TINYINT", "label": "TINYINT" },
-              { "value": "SMALLINT", "label": "SMALLINT" },
-              { "value": "MEDIUMINT", "label": "MEDIUMINT" },
-              { "value": "INT", "label": "INT" },
-              { "value": "BIGINT", "label": "BIGINT" },
-              { "value": "FLOAT", "label": "FLOAT" },
-              { "value": "DOUBLE", "label": "DOUBLE" },
-              { "value": "DECIMAL", "label": "DECIMAL" },
-              { "value": "NUMERIC", "label": "NUMERIC" },
-              { "value": "BIT", "label": "BIT" },
-              { "value": "DATE", "label": "DATE" },
-              { "value": "TIME", "label": "TIME" },
-              { "value": "DATETIME", "label": "DATETIME" },
-              { "value": "TIMESTAMP", "label": "TIMESTAMP" },
-              { "value": "YEAR", "label": "YEAR" },
-              { "value": "VARCHAR(255)", "label": "VARCHAR(255)" },
-              { "value": "CHAR(10)", "label": "CHAR(10)" },
-              { "value": "BINARY", "label": "BINARY" },
-              { "value": "VARBINARY", "label": "VARBINARY" },
-              { "value": "BLOB", "label": "BLOB" },
-              { "value": "TEXT", "label": "TEXT" },
-              //Enum と Set はUIを考えてから 
-            ]}
+            options={typeChoices}
           />
         )}
       />
---
+++ src/components/typeChoices.js
@@ -0,0 +1,26 @@
+const typeChoices = [
+  { "value": "TINYINT", "label": "TINYINT" },
+  { "value": "SMALLINT", "label": "SMALLINT" },
+  { "value": "MEDIUMINT", "label": "MEDIUMINT" },
+  { "value": "INT", "label": "INT" },
+  { "value": "BIGINT", "label": "BIGINT" },
+  { "value": "FLOAT", "label": "FLOAT" },
+  { "value": "DOUBLE", "label": "DOUBLE" },
+  { "value": "DECIMAL", "label": "DECIMAL" },
+  { "value": "NUMERIC", "label": "NUMERIC" },
+  { "value": "BIT", "label": "BIT" },
+  { "value": "DATE", "label": "DATE" },
+  { "value": "TIME", "label": "TIME" },
+  { "value": "DATETIME", "label": "DATETIME" },
+  { "value": "TIMESTAMP", "label": "TIMESTAMP" },
+  { "value": "YEAR", "label": "YEAR" },
+  { "value": "VARCHAR(255)", "label": "VARCHAR(255)" },
+  { "value": "CHAR(10)", "label": "CHAR(10)" },
+  { "value": "BINARY", "label": "BINARY" },
+  { "value": "VARBINARY", "label": "VARBINARY" },
+  { "value": "BLOB", "label": "BLOB" },
+  { "value": "TEXT", "label": "TEXT" },
+  //Enum と Set はUIを考えてから 
+];
+
+export default typeChoices;
